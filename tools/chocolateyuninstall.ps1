cecff3999b9098d25a72ef1fd704740a9dcf6e47
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-07-09 18:11:16 -0400

    Formatting cleanup

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,4 +1,5 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
+
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Twinkle Tray*'
@@ -9,14 +10,19 @@ $packageArgs = @{
 
 [array] $key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
-if ($key.Count -eq 1) {
+if ($key.Count -eq 1)
+{
   $key | ForEach-Object {
     $packageArgs['file'] = $($_.UninstallString.TrimEnd('/currentuser'))
     Uninstall-ChocolateyPackage @packageArgs
   }
-} elseif ($key.Count -eq 0) {
+}
+elseif ($key.Count -eq 0)
+{
   Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
+}
+elseif ($key.Count -gt 1)
+{
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
@@ -28,4 +34,3 @@ $installedApplicationPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath 'Progra
                             Join-Path -ChildPath 'Twinkle Tray.exe'
 
 Uninstall-BinFile -Name 'twinkletray' -Path $installedApplicationPath
-
