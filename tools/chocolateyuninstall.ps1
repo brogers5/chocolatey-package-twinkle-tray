7162a6ebb4257bf52ec08e007252cdbebff0978a
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-07-09 18:13:33 -0400

    Rename keys

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -8,11 +8,11 @@ $packageArgs = @{
   silentArgs     = '/currentuser /S'
 }
 
-[array] $key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+[array] $keys = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
-if ($key.Count -eq 1)
+if ($keys.Count -eq 1)
 {
-  $key | ForEach-Object {
+  $keys | ForEach-Object {
     $packageArgs['file'] = $($_.UninstallString.TrimEnd('/currentuser'))
     Uninstall-ChocolateyPackage @packageArgs
   }
@@ -21,12 +21,12 @@ elseif ($key.Count -eq 0)
 {
   Write-Warning "$packageName has already been uninstalled by other means."
 }
-elseif ($key.Count -gt 1)
+elseif ($keys.Count -gt 1)
 {
-  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "$($keys.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
+  $keys | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
 }
 
 $installedApplicationPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath 'Programs' |
