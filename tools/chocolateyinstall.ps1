e0ad49dd96ea26c801dbc9eb513263ba48f39795
brogers5 <6869577+brogers5@users.noreply.github.com>
2024-10-02 18:31:02 -0400

    Update to 1.16.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$installerFileName = 'Twinkle.Tray.v1.16.0.exe'
+$installerFileName = 'Twinkle.Tray.v1.16.1.exe'
 $filePath = Join-Path -Path $toolsDir -ChildPath $installerFileName
 $softwareNamePattern = 'Twinkle Tray*'
 
